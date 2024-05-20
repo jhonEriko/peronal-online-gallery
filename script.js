d1e843fd0a4b2f8909424d75402af386bad4c3bc
Eriko Teston <erikoteston@gmail.com>
2024-05-21 00:17:00 +0800

    finalized everything

--- script.js
+++ script.js
@@ -1,32 +1,13 @@
 let uploadImage = document.getElementById("input");
 let imageContainer = document.getElementById("image-container");
-let galleryContainer = document.getElementById("gallery-container")
+let galleryContainer = document.getElementById("gallery-container");
+let columnContainer = document.getElementById("column-container");
 let oddColumn = document.getElementById("odd-column");
 let evenColumn = document.getElementById("even-column");
+let thirdColumn = document.getElementById("third-column");
 const addedImages = [];	
 
-
-
-
-/*input.onchange = () => {
-	//uploadedImage.src = URL.createObjectURL(uploadImage.files[0]);
-	let newImage = document.createElement("img");
-	newImage.src = URL.createObjectURL(uploadImage.files[0]);
-	imageContainer.appendChild(newImage);
-}*/
-
-/*input.onchange = () => {
-	let newImage = document.createElement("img");
-	newImage.src = URL.createObjectURL(uploadImage.files[0]);
-	if (addedImages.length % 2 === 0) {
-		evenColumn.appendChild(newImage);
-		addedImages[addedImages.length] = addedImages.length;
-	} else if (addedImages.length % 2 !== 0) {
-		oddColumn.appendChild(newImage);
-		addedImages[addedImages.length] = addedImages.length;
-	};
-}*/
-
+//Adding an image to the gallery
 input.onchange = () => {
 	let imageDiv = document.createElement("div");
 	let newImage = document.createElement("img");
@@ -34,50 +15,23 @@ input.onchange = () => {
 	imageDiv.appendChild(newImage);
 	newImage.src = URL.createObjectURL(uploadImage.files[0]);
 
-	if (addedImages.length % 2 === 0) {
+	if (getComputedStyle(thirdColumn)["display"] == "block") {
+		if (addedImages.length % 3 === 0) {
+			evenColumn.appendChild(imageDiv);
+			addedImages[addedImages.length] = addedImages.length;
+		}else if (addedImages.length % 2 === 0) {
+			thirdColumn.appendChild(imageDiv);
+			addedImages[addedImages.length] = addedImages.length;
+		}else {
+			oddColumn.appendChild(imageDiv);
+			addedImages[addedImages.length] = addedImages.length;
+		}
+	}else if (addedImages.length % 2 === 0) {
 		evenColumn.appendChild(imageDiv);
 		addedImages[addedImages.length] = addedImages.length;
-	} else if (addedImages.length % 2 !== 0) {
+	}else if (addedImages.length % 2 !== 0) {
 		oddColumn.appendChild(imageDiv);
 		addedImages[addedImages.length] = addedImages.length;
-	};
-
-	/*deleteImage.addEventListener("click", (e) => {
-		if (confirm("Delete photo?") == true) {
-			e.target.parentElement.remove();
-			addedImages.pop();
-			uploadImage.value = "";
-		};
-	});*/
-
-	newImage.addEventListener("click", (e) => {
-		console.log(uploadImage.files[0].name);
-		let viewImage = document.createElement("div");
-		viewImage.classList.add("view-image");
-		document.body.appendChild(viewImage);
-		viewImage.style.display = "block";
-		viewImage.style.position = "fixed";
-		let imageHolder = document.createElement("div");
-		imageHolder.classList.add("image-holder");
-		viewImage.appendChild(imageHolder);
-		let closeImage = document.createElement("span");
-		closeImage.innerHTML = "&#10060";
-		imageHolder.appendChild(closeImage);
-		let deleteImage = document.createElement("span");
-		deleteImage.classList.add("delete");
-		deleteImage.innerHTML = "Delete";
-		imageHolder.appendChild(deleteImage);
-		let imageViewed = document.createElement("img");
-		imageViewed.src = URL.createObjectURL(uploadImage.files[0]);
-		imageHolder.appendChild(imageViewed);
-
-		window.onclick = (e) => {
-			console.log(viewImage.tagName);
-			if (e.target.tagName = "DIV") {
-				viewImage.style.display = "none";
-			}
-		}
-			
-	});
-}
+	}  
+};
 
